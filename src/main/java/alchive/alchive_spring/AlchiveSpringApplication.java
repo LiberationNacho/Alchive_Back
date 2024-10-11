b7484cf6c5cf5e08a995962e9f17d7c5967b6482
henac <man25816@naver.com>
2024-10-11 21:15:23 +0900

    first commit

---
+++ src/main/java/alchive/alchive_spring/AlchiveSpringApplication.java
@@ -0,0 +1,13 @@
+package alchive.alchive_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlchiveSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlchiveSpringApplication.class, args);
+	}
+
+}
