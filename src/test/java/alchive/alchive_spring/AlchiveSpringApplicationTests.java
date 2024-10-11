d38809c92dc03b6ef44cb221d300fc8895dd87e9
henac <man25816@naver.com>
2024-10-11 21:15:23 +0900

    first commit

---
+++ src/test/java/alchive/alchive_spring/AlchiveSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package alchive.alchive_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlchiveSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
